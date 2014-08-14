5bd44f20b3db9586b888fe6318c28d673080fc59
Jose Contreras <jocontre@joselcontreras.com>
2014-08-13 20:50:49 -0700

    Refactoring

--- ball.js
+++ ball.js
@@ -7,16 +7,35 @@ function Ball () {
 	this.x = game.width/2 - this.width;
 	this.y = game.height/2 - this.height;
 
-	this.yVelocity = 10;
+	this.reset();
 }
 
 Ball.prototype = Object.create(Entity.prototype);
 Ball.prototype.constructor = Ball;
+
+Ball.prototype.reset = function(){
+	this.x = game.width/2 - this.width;
+	this.y = game.height/2 - this.height;
+
+	var min = -5,
+		max = 5;
+
+	this.xVelocity = Math.floor(Math.random()*(max - min + 1) + min);
+	this.yVelocity = Math.random() > 0.5 ? 5 : -5 // ternary
+}
 
 Ball.prototype.update = function(){
 	Entity.prototype.update.apply(this, arguments)// overriding the update method.
 
 	if(this.y > game.height - this.height || this.y < 0){ 
 		this.yVelocity *= -1 //if ball goes offscreen, reverse direction (vector).
+	}
+
+	if(this.x > game.width){
+		this.reset()
+	}
+
+	if(this.x < 0){
+		this.reset()
 	}
 }
--- pong.js
+++ pong.js
@@ -4,15 +4,17 @@ function Background () {
 
 Background.prototype.draw = function(context) {
 	context.fillStyle = '#000000';
-	context.fillRect(0,0,game.width, game.height);
+	context.fillRect(0, 0, game.width, game.height);
 };
 
 var canvas = $('canvas')[0],  //grab the canvas element on the page
 	game = new Game(canvas);
 
 game.entities = [
-	new Background();
-	new Ball();
+	new Background(),
+	game.ball = new Ball(),
+	game.player = new Player(),
+	game.bot = new Bot()
 ];
 
 game.start();
