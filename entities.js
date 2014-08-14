e42f0fa2e4804777b192943df290b8543f0933ce
Jose Contreras <jocontre@joselcontreras.com>
2014-08-13 19:17:24 -0700

    Creating paddle and ball with movement

---
+++ entities.js
@@ -0,0 +1,30 @@
+function Entity () {
+	this.x = 0;
+	this.y = 0;
+
+	this.width = 0;
+	this.height = 0;
+
+	this.xVelocity = 0;
+	this.yVelocity = 0;
+};
+
+Entity.prototype.update = function(){
+	this.x += this.xVelocity;
+	this.y += this.yVelocity;
+};
+
+Entity.prototype.draw = function(context){
+	context.fillStyle = '#ffffff';
+	context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+// Collision detection (the significant part)!
+// Return true if there is intersection between the paddle and ball
+Entity.prototype.intersect = function(other){
+	return this.y + this.height > other.y &&
+			this.y 				< other.y + other.height &&
+			this.x + this.width > other.x &&
+			this.x 				< other.x + other.width;
+};
+
--- pong.js
+++ pong.js
@@ -1,2 +1,19 @@
+function Background () {
+	
+};
+
+Background.prototype.draw = function(context) {
+	context.fillStyle = '#000000';
+	context.fillRect(0,0,game.width, game.height);
+};
+
 var canvas = $('canvas')[0],  //grab the canvas element on the page
 	game = new Game(canvas);
+
+game.entities = [
+	new Background();
+	new Ball();
+];
+
+game.start();
+game.focus();
